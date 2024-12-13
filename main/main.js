f9894ab5e54866bb0bed6319bc685cbae67347a9
federicomolino <federicomolino450@gmail.com>
2024-12-13 19:50:45 +0100

    add: aggiunto stile messaggi errore

--- main/main.js
+++ main/main.js
@@ -16,7 +16,8 @@ submitBotton.addEventListener('click', function(event){
     //verfico che entrambi siano numeri quelli inseriti
     if(isNaN(numberKm) || isNaN (agePerson) || numberKm <= 0 || agePerson <= 0){
 
-        resultInput.innerHTML = `Entrabe le richieste devono avere un valore numerico e superiore a 0, grazie`;
+        resultInput.innerHTML = `Entrabe le richieste devono avere un valore numerico superiore a 0, grazie`;
+        resultInput.classList.add('text-red')
 
     }else{
 
@@ -29,7 +30,7 @@ submitBotton.addEventListener('click', function(event){
             priceFinal = priceFinal - ((priceTot * 20)/100);
   
             //stampo prezzo finito con max 2 decimali e cambio il punto con la virgola, con sconto Min 18
-            resultInput.innerHTML = `€${priceFinal.toFixed(2).replace(".",",")}`;
+            resultInput.innerHTML = `Il costo totale con sconto 20% :  €${priceFinal.toFixed(2).replace(".",",")}`;
 
 
         //controllo che l'età sia >= di 65 + calcolo
@@ -37,12 +38,12 @@ submitBotton.addEventListener('click', function(event){
             priceFinal = priceFinal - (priceTot * 40)/100;
 
             //stampo prezzo finito con max 2 decimali e cambio il punto con la virgola, con sconto 0ver 65
-           resultInput.innerHTML = `€${priceFinal.toFixed(2).replace(".",",")}`;
+           resultInput.innerHTML = `Il costo totale con sconto 40% :  €${priceFinal.toFixed(2).replace(".",",")}`;
 
         }else{
             
             //stampo prezzo finito con max 2 decimali e cambio il punto con la virgola
-            resultInput.innerHTML = `€${priceFinal.toFixed(2).replace(".",",")}`;
+            resultInput.innerHTML = `Il costo totale :  €${priceFinal.toFixed(2).replace(".",",")}`;
         }
     }
 
